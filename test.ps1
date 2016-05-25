7a758d8c4c5820643b6aea7f03e4d021bfcccbe4
Rasmus Oudal Edberg <re@cryosinternational.com>
2016-05-25 15:54:05 +0200

    Added build and test scripts.

---
+++ test.ps1
@@ -0,0 +1,31 @@
+$PSScriptFilePath = (Get-Item $MyInvocation.MyCommand.Path).FullName
+
+" PSScriptFilePath = $PSScriptFilePath"
+
+$SolutionRoot = Split-Path -Path $PSScriptFilePath -Parent
+$ProjectJsonPath = Join-Path -Path $SolutionRoot -ChildPath "test\CQRSCore.Tests\project.json"
+
+$DOTNET = "dotnet"
+
+& $DOTNET --info
+
+& $DOTNET restore
+
+#& $DOTNET restore "$ProjectJsonPath"
+#if (-not $?)
+#{
+#	throw "The dotnet restore process returned an error code."
+#}
+
+#& $DOTNET build "$ProjectJsonPath"
+#if (-not $?)
+#{
+#	throw "The dotnet build process returned an error code."
+#}
+
+# run them
+& $DOTNET test "$ProjectJsonPath"
+if (-not $?)
+{
+	throw "The dotnet test process returned an error code."
+}
