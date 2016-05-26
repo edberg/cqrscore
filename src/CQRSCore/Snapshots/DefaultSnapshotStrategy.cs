1f3c09170af8ba4da2e546017409d3134a1e0e59
Rasmus Oudal Edberg <re@cryosinternational.com>
2016-05-26 14:58:29 +0200

    Fixed bug

--- src/CQRSCore/Snapshots/DefaultSnapshotStrategy.cs
+++ src/CQRSCore/Snapshots/DefaultSnapshotStrategy.cs
@@ -10,12 +10,11 @@ namespace CQRSCore.Snapshots
         private const int SnapshotInterval = 100;
         public bool IsSnapshotable(Type aggregateType)
         {
-            if (aggregateType.GetTypeInfo().BaseType == null)
-                return false;
+            if (aggregateType.GetTypeInfo().BaseType == null) return false;
             if (aggregateType.GetTypeInfo().BaseType.GetTypeInfo().IsGenericType &&
                 aggregateType.GetTypeInfo().BaseType.GetGenericTypeDefinition() == typeof(SnapshotAggregateRoot<>))
                 return true;
-            return IsSnapshotable(aggregateType.GetType().GetTypeInfo().BaseType);
+            return IsSnapshotable(aggregateType.GetTypeInfo().BaseType);
         }
 
         public bool ShouldMakeSnapShot(AggregateRoot aggregate)
